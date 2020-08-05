0fc21cc8e49b6e6bd510e033b7d3ce8eb9ee2dc7
SC8007889919 <SSC8007889919@outlook.com>
2020-08-05 17:58:28 +0530

    Crud

---
+++ build/precache-manifest.9e74bc09d38b846f488a2be5b3a05bc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f3720b75f47a0369184c984b38101e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54ce7eb9cf82c8b160fe",
+    "url": "/static/css/main.d0c34bf1.chunk.css"
+  },
+  {
+    "revision": "127e8dda232cbd2e10fe",
+    "url": "/static/js/2.63c24e5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.63c24e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ce7eb9cf82c8b160fe",
+    "url": "/static/js/main.e308b4d6.chunk.js"
+  },
+  {
+    "revision": "46e4d14e8c6233c46512",
+    "url": "/static/js/runtime-main.b3b50565.js"
+  }
+]);
